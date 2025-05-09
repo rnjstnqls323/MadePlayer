086f46dfa0cb027b21ec88e5e8216e1ba39bad27
donghalee1234 <leedongha010101@gmail.com>
2025-05-09 13:05:20 +0900

    FrameWork 및 파일 이름 수정

--- TeamProject_2/framework.h
+++ TeamProject_2/framework.h
@@ -5,11 +5,21 @@
 #pragma once
 
 #include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
-// Windows 헤더 파일
+#define WIN32_LEAN_AND_MEAN  
+
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 800
+
+#define PI 3.141592f
+
+#define DELTA Timer::Get()->GetElapsedTime()
 #include <windows.h>
-// C 런타임 헤더 파일입니다.
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "Utilities/Singleton.h"
+#include "Utilities/Timer.h"
+#include "Utilities/Vector2.h"
+#include "Objects/Circle.h"
+#include "Objects/Bullet.h"
